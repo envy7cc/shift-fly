6c153f8fda04fb932700725d4ee7520864b95ad8
Max <Max@DESKTOP-V0RUP73.Speedport_W_724V_09011603_06_003>
2019-09-29 18:59:59 +0200

    added init

---
+++ ShiftFly/src/net/minefall/shiftfly/ShiftFly.java
@@ -0,0 +1,17 @@
+package net.minefall.shiftfly;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ShiftFly extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+
+}
